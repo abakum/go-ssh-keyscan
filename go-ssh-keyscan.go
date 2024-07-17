d6d9feb86158d025b85f64a61d612d9a1f6f6784
abakum <koka.abakum@gmail.com>
2024-07-17 23:10:06 +0300

    init

--- go-ssh-keyscan.go
+++ go-ssh-keyscan.go
@@ -2,37 +2,53 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"io"
 	"log"
 	"net"
 	"os"
+	"strings"
 	"sync"
+	"time"
 
+	"github.com/abakum/pageant"
+	"github.com/skeema/knownhosts"
 	"golang.org/x/crypto/ssh"
 	"golang.org/x/crypto/ssh/agent"
 )
 
 const (
-	Username    = "sszuecs"
-	DefaultPort = 22
+	Scan        = "scan"
+	DefaultPort = "22"
+	TO          = time.Second * 2
 )
 
-var Ch chan string = make(chan string)
+var Ch chan string = make(chan string, 10)
 
 func KeyScanCallback(hostname string, remote net.Addr, key ssh.PublicKey) error {
-	Ch <- fmt.Sprintf("%s %s", hostname[:len(hostname)-3], string(ssh.MarshalAuthorizedKey(key)))
-	return nil
+	Ch <- fmt.Sprintf("%s %s", knownhosts.Normalize(hostname), string(ssh.MarshalAuthorizedKey(key)))
+	return fmt.Errorf(Scan)
 }
 
-func dial(server string, config *ssh.ClientConfig, wg *sync.WaitGroup) {
-	_, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", server, DefaultPort), config)
-	if err != nil {
-		log.Fatalln("Failed to dial:", err)
-	}
-	wg.Done()
+func dial(hp string, config *ssh.ClientConfig, wg *sync.WaitGroup) (HostKeyAlgorithms []string) {
+	defer wg.Done()
 
+	client, err := ssh.Dial("tcp", hp, config)
+	if err != nil {
+		// log.Println("Failed to dial:", err)
+		ss := strings.Split(err.Error(), "server offered: [")
+		if len(ss) > 1 {
+			ss = strings.Split(ss[1], "]")
+			if len(ss) > 1 {
+				HostKeyAlgorithms = strings.Fields(ss[0])
+			}
+		}
+
+		return
+	}
+	// log.Println("success dial", hp, config)
+	defer client.Close()
+	return
 }
 
 func out(wg *sync.WaitGroup) {
@@ -43,35 +59,102 @@ func out(wg *sync.WaitGroup) {
 }
 
 func main() {
-	auth_socket := os.Getenv("SSH_AUTH_SOCK")
-	if auth_socket == "" {
-		log.Fatal(errors.New("no $SSH_AUTH_SOCK defined"))
-	}
-	conn, err := net.Dial("unix", auth_socket)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
-	ag := agent.NewClient(conn)
-	auths := []ssh.AuthMethod{ssh.PublicKeysCallback(ag.Signers)}
+	log.SetFlags(log.Lshortfile)
 
-	config := &ssh.ClientConfig{
-		User:            Username,
-		Auth:            auths,
-		HostKeyCallback: KeyScanCallback,
+	auths := []ssh.AuthMethod{}
+	conn, err := pageant.NewConn()
+	if err == nil {
+		defer conn.Close()
+		ag := agent.NewClient(conn)
+		auths = []ssh.AuthMethod{ssh.PublicKeysCallback(ag.Signers)}
 	}
 
 	var wg sync.WaitGroup
 	go out(&wg)
+	body := func(uhp string) {
+		h, p, err := net.SplitHostPort(uhp)
+		if err != nil {
+			if !strings.HasSuffix(err.Error(), "missing port in address") {
+				log.Println("parsing", uhp, err)
+				return
+			}
+			h = uhp
+			p = DefaultPort
+		}
+		ss := strings.Split(h, "@")
+		u := Scan
+		if len(ss) > 1 {
+			u = ss[0]
+			h = ss[1]
+		}
+		// log.Println(u+"@"+h+":"+p, HostKeyAlgorithms)
+		wg.Add(1) // dial and print
+		HostKeyAlgorithms := dial(h+":"+p, &ssh.ClientConfig{
+			User:              u,
+			Auth:              auths,
+			HostKeyCallback:   KeyScanCallback,
+			HostKeyAlgorithms: []string{Scan},
+			Timeout:           TO,
+			BannerCallback: func(banner string) error {
+				wg.Add(1)
+				Ch <- fmt.Sprintf("# %s %s", h+":"+p, banner)
+				return nil
+			},
+		}, &wg)
+		wg.Add(1)
+		Ch <- fmt.Sprintf("# %s@%s:%s %v\n", u, h, p, HostKeyAlgorithms)
+		if len(HostKeyAlgorithms) == 0 {
+			HostKeyAlgorithms = []string{
+				ssh.CertAlgoRSASHA256v01, ssh.CertAlgoRSASHA512v01,
+				ssh.CertAlgoRSAv01, ssh.CertAlgoDSAv01, ssh.CertAlgoECDSA256v01,
+				ssh.CertAlgoECDSA384v01, ssh.CertAlgoECDSA521v01, ssh.CertAlgoED25519v01,
+
+				ssh.KeyAlgoECDSA256, ssh.KeyAlgoECDSA384, ssh.KeyAlgoECDSA521,
+				ssh.KeyAlgoRSASHA256, ssh.KeyAlgoRSASHA512,
+				ssh.KeyAlgoRSA, ssh.KeyAlgoDSA,
+
+				ssh.KeyAlgoED25519,
+			}
+		}
+		CertAlgoRSA := false
+		KeyAlgoRSA := false
+		for _, HostKeyAlgorithm := range HostKeyAlgorithms {
+			switch HostKeyAlgorithm {
+			case ssh.CertAlgoRSASHA256v01, ssh.CertAlgoRSASHA512v01, ssh.CertAlgoRSAv01:
+				if CertAlgoRSA {
+					continue
+				}
+				CertAlgoRSA = true
+			case ssh.KeyAlgoRSASHA256, ssh.KeyAlgoRSASHA512, ssh.KeyAlgoRSA:
+				if KeyAlgoRSA {
+					continue
+				}
+				KeyAlgoRSA = true
+			}
+			wg.Add(2) // dial and print
+			go dial(h+":"+p, &ssh.ClientConfig{
+				User:              u,
+				Auth:              auths,
+				HostKeyCallback:   KeyScanCallback,
+				HostKeyAlgorithms: []string{HostKeyAlgorithm},
+				Timeout:           TO,
+			}, &wg)
+		}
+	}
+	fmt.Fprintln(os.Stderr, "Enter [user@]host[:port]")
+	if len(os.Args) > 1 {
+		fmt.Fprintln(os.Stderr, "Read [user@]host[:port] from os.Args")
+		for _, uhp := range os.Args[1:] {
+			body(strings.TrimSpace(uhp))
+		}
+	}
 	reader := bufio.NewReader(os.Stdin)
 	for {
-		server, err := reader.ReadString('\n')
+		uhp, err := reader.ReadString('\n')
 		if err == io.EOF {
 			break
 		}
-		server = server[:len(server)-1] // chomp
-		wg.Add(2)                       // dial and print
-		go dial(server, config, &wg)
+		body(strings.TrimSpace(uhp))
 	}
 	wg.Wait()
 }
---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/abakum/go-ssh-keyscan
+
+go 1.21.0
+
+require (
+	github.com/abakum/pageant v0.0.1
+	github.com/skeema/knownhosts v1.3.0
+	golang.org/x/crypto v0.25.0
+)
+
+require (
+	github.com/Microsoft/go-winio v0.6.1 // indirect
+	golang.org/x/mod v0.8.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/tools v0.6.0 // indirect
+)
